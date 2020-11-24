127d36c985418500c797e7be05b945cc701d2691
Yuyi Zhang <yuyizhg@gmail.com>
2020-11-23 22:47:59 -0800

    version 3

--- 1480_running_sum_of_1d_array.py
+++ 1480_running_sum_of_1d_array.py
@@ -3,10 +3,11 @@ nums = [1, 2, 3, 4]
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
 
-        num = [None] * len(nums)
-        num[0] = nums[0]
+        runningSum = [None] * len(nums)
+        runningSum[0] = nums[0]
         for index, value in enumerate(nums):
             if index != 0:
-                num[index] = num[(index - 1)] + nums[index]
+                runningSum[index] = runningSum[(index - 1)] + nums[index]
+                index = index + 1
 
-        return num
+        return runningSum
