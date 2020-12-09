0a245746018b7beb6ad0a5a8c19ac24af85bc3df
Yuyi Zhang <yuyizhg@gmail.com>
2020-12-08 23:39:44 -0800

    511. Game Play Analysis I

--- Database/1173_immediate_food_delivery_i.sql
+++ Database/1173_immediate_food_delivery_i.sql
@@ -1,2 +1,2 @@
-SELECT ROUND(100 * SUM(order_date = customer_pref_delivery_date) / COUNT(delivery_id), 2) AS immediate_percentage
+SELECT ROUND(100 * AVG(order_date = customer_pref_delivery_date), 2) AS immediate_percentage
 FROM Delivery;
---
+++ Database/511_game_play_analysis_i.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT(player_id) AS player_id, MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id;
